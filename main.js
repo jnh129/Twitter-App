e45f74175f4fda39ec819918fe518980b65c1ae0
jnh129 <jonah.n.henry@gmail.com>
2020-10-21 16:07:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+"use strict";
+let express = require("express"),
+	login = require("./modules/loginModule.js"),
+	parser = require("body-parser"),
+	app = express();
+app.use(parser.urlencoded({extended: false}));
+app.use(parser.json());
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+
+let Twitter = require("twitter");
+
+let client = new Twitter({
+	consumer_key: "q6iTAeEA7SxGtxM1aOl7be1wZ",
+	consumer_secret: "u2zCUlunXOuKhcWim3fnxQFKCKs2shrVM8CfSyragY6JioCdTQ",
+	access_token_key: "385595015-x9iLZvXjUSsoidsYNqsVeI4W5s1T6bCBlfvk8qyH",
+	access_token_secret: "pesvcnhUjeiBsxDYgt81AMDIKBKiqglQbvOe1oSb0itfB"
+});
+-
+app.get("/", function(request,response){ 
+	let obj = { 
+		title: "Login",
+		bottomMessage: "Sign in for exclusive access!",
+		homeTab: "Login",
+		secondaryTab: "Create Account", 
+		submitButton: "DO IT",
+		homeTabPath: "/",
+		secondaryTabPath: "/create_account",
+		httpAction: "/login_attempt"
+	}; 
+	response.render("login.pug",obj); 
+});
+
+app.get("/create_account", function(request,response){
+	let obj = {       
+		title: "Create Account",
+		bottomMessage: "Select DONE when you are finished",
+		homeTab: "Create Account",
+		secondaryTab: "Login", 
+		submitButton: "DONE",
+		homeTabPath: "/create_account",
+		secondaryTabPath: "/",
+		httpAction: "/register_attempt"
+	};
+	response.render("login.pug",obj);
+});
+
+
+app.get("/getTweets", function(request, response){
+	client.get("search/tweets",{q: "#csc365"}, function(error, tweets) {
+		let result = [];
+		if (!error) {
+			tweets.statuses.forEach(function(tweet){
+				result.push({"text": tweet.text, "followers": tweet.user.followers_count});
+			});
+		}
+		else{console.log(error);}
+		response.json(result);
+	});
+});
+
+app.post("/login_attempt", function(request, response){
+	let username = request.body.user;
+	let password = request.body.password;
+	if(login.loginCheck(username, password)){
+		response.render("goodjob.pug");
+	}
+});
+
+app.post("/register_attempt", function(request, response){
+	let username = request.body.user;
+	let password = request.body.password;
+	login.register(username, password);
+	response.render("goodjob.pug");
+});
+
+app.listen(3000, function() {
+	console.log("Listening on port 3000");
+});
+app.use(express.static("resources"));
